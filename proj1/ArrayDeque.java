9df1ef3b42957c94eb508bbc0c6f3dbcd42b2475
Max <rustycrusty01@gmail.com>
2016-03-16 23:02:41 +0300

    CS 61B: Project1

--- proj1/ArrayDeque.java
+++ proj1/ArrayDeque.java
@@ -1,63 +1,110 @@
-/**
- * Created by max on 3/10/16.
- */
 public class ArrayDeque<Item> {
+    private Item[] items;
+    private int size;
+    private static int RFACTOR = 2;
 
 
-    public void addFirst(Item) {
+
+    public ArrayDeque() {
+        size = 0;
+        items = (Item[]) new Object[8];
+
+    }
+
+    private void resize(int capacity) {
+
+        Item[] fresh = (Item[]) new Object[capacity];
+        System.arraycopy(items, 0, fresh, 0, size);
+        items = fresh;
+    }
+
     /*
     Adds an item to the front of the Deque.
      */
+    public void addFirst(Item i) {
+
+        items[0] = i;
+        size++;
     }
 
-    public void addLast(Item) {
     /*
     Adds an item to the back of the Deque.
      */
+    public void addLast(Item i) {
+
+        if (size == items.length) {
+            resize(size * RFACTOR);
+        }
+
+        items[size] = i;;
+        size++;
     }
 
-    public boolean isEmpty() {
     /*
     Returns true if deque is empty, false otherwise.
      */
-        return false;
+    public boolean isEmpty() {
+
+        return size==0;
     }
 
-    public int size() {
     /*
     Returns the number of items in the Deque.
      */
-        return 0;
+    public int size() {
+
+        return size;
     }
 
-    public void printDeque() {
     /*
     Prints the items in the Deque from first to last, separated by a space.
      */
+    public void printDeque() {
+
+        for (int i = 0; i < this.size(); i++) {
+            System.out.print(this.get(i) + " ");
+        }
     }
 
-    public Item removeFirst() {
     /*
     Removes and returns the item at the front of the Deque.
     If no such item exists, returns null.
      */
+    public Item removeFirst() {
+
         return null;
     }
 
-    public Item removeLast() {
+    /*
+    Returns the item at the back of the Deque.
+    If no such item exists, returns null.
+     */
+    private Item getBack() {
+
+        return items[size - 1];
+    }
+
     /*
     Removes and returns the item at the back of the Deque.
     If no such item exists, returns null.
      */
-        return null;
+    public Item removeLast() {
+
+        Item last = getBack();
+        items[size - 1] = null;
+        size--;
+        return last;
     }
 
-    public Item get(int index) {
     /*
     Gets the item at the given index, where 0 is the front, 1 is the next item,
     and so forth. If no such item exists, returns null. Must not alter the deque!
      */
-        return null;
+    public Item get(int index) {
+
+        if (index > size) return null;
+
+        return items[index];
     }
 
 
--- proj1/LinkedListDeque.java
+++ proj1/LinkedListDeque.java
@@ -1,74 +1,134 @@
-/**
- * Created by max on 3/10/16.
- */
 public class LinkedListDeque<Item> {
 
+    public class Node {
+        public Item item;
+        public Node next;
+        public Node prev;
+
+        public Node(Item i, Node n, Node p) {
+            item = i;
+            next = n;
+            prev = p;
+        }
+    }
+
+    private Node sentinel;
+    private int size;
+
     public LinkedListDeque() {
+        size = 0;
+        sentinel = new Node(null, sentinel, sentinel);
 
     }
 
-    public void addFirst(Item) {
     /*
     Adds an item to the front of the Deque.
      */
+    public void addFirst(Item i) {
+
+        if (size == 0) {
+            sentinel.next = new Node(i, sentinel, sentinel);
+            sentinel.prev = sentinel.next;
+            size++;
+        } else {
+            Node oldnext = sentinel.next;
+            sentinel.next = new Node(i, sentinel, oldnext);
+            oldnext.prev = sentinel.next;
+            size++;
+        }
     }
 
-    public void addLast(Item) {
     /*
     Adds an item to the back of the Deque.
      */
+    public void addLast(Item i) {
+
+        if (size == 0) {
+            sentinel.next = new Node(i, sentinel, sentinel);
+            sentinel.prev = sentinel.next;
+            size++;
+        } else {
+            Node oldprev = sentinel.prev;
+            sentinel.prev = new Node(i, oldprev, sentinel);
+            oldprev.next = sentinel.prev;
+            size++;
+        }
     }
 
-    public boolean isEmpty() {
     /*
     Returns true if deque is empty, false otherwise.
      */
-        return false;
+    public boolean isEmpty() {
+
+        return size == 0;
     }
 
-    public int size() {
     /*
     Returns the number of items in the Deque.
      */
-        return 0;
+    public int size() {
+
+        return size;
     }
 
-    public void printDeque() {
     /*
     Prints the items in the Deque from first to last, separated by a space.
      */
+    public void printDeque() {
+
+        for (int i = 0; i < this.size(); i++) {
+            System.out.print(this.get(i) + " ");
+        }
     }
 
-    public Item removeFirst() {
     /*
     Removes and returns the item at the front of the Deque.
     If no such item exists, returns null.
      */
-        return null;
+    public Item removeFirst() {
+
+        Node oldfirst = sentinel.next;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size--;
+        return oldfirst.item;
     }
 
-    public Item removeLast() {
     /*
     Removes and returns the item at the back of the Deque.
     If no such item exists, returns null.
      */
-        return null;
+    public Item removeLast() {
+
+        Node oldlast = sentinel.prev;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size--;
+        return oldlast.item;
     }
 
-    public Item get(int index) {
     /*
     Gets the item at the given index, where 0 is the front, 1 is the next
     item, and so forth. If no such item exists, returns null.
     Must not alter the deque!
      */
-        return null;
+    public Item get(int index) {
+
+        if (size == 0 || index > size) return null;
+
+        Node old = sentinel.next;
+        for (int i = 0; i < index; i++) {
+            old = old.next;
+        }
+        return old.item;
     }
 
-    public Item getRecursive(int index) {
     /*
     Gets the item at the given index, where 0 is the front, 1 is the next item,
-    and so forth. If no such item exists, returns null. Must not alter the deque!
+    and so forth. If no such item exists, returns null.
+    Must not alter the deque!
      */
+    public Item getRecursive(int index) {
         return null;
     }
 
