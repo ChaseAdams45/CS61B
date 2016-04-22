fd9464349693d82fa28857849b472c6dd49bd890
Max <rustycrusty01@gmail.com>
2016-04-22 20:55:57 +0300

    lab2, lab3 done

---
+++ lab3/Flik/Flik.java
@@ -0,0 +1,9 @@
+/** An Integer tester created by Flik Enterprises. */
+public class Flik {
+    public static boolean isSameNumber(Integer a, Integer b) {
+        // Should use equals() method to compare objects
+        // or use int primitive in arguments.
+
+        return a == b;
+    }
+}
